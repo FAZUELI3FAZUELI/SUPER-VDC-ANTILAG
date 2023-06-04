cc6c81204e90275908e43215b8ce6e429f834831
FAZUELI3FAZUELI <123437534+FAZUELI3FAZUELI@users.noreply.github.com>
2023-06-03 22:12:14 -0300

    Negao-antilag.lua

---
+++ Negao-antilag.lua
@@ -0,0 +1,77 @@
+local runService = game:GetService("RunService")
+
+local lighting = game:GetService("Lighting")
+
+-- Defina as configurações gráficas para valores mais baixos
+
+lighting.GlobalShadows = false
+
+lighting.Outlines = false
+
+lighting.Brightness = 0.5
+
+-- Remova elementos desnecessários que podem causar lag
+
+-- Exemplo: Remova todas as partículas no jogo
+
+for _, particleEmitter in ipairs(game:GetService("Workspace"):GetDescendants()) do
+
+    if particleEmitter:IsA("ParticleEmitter") then
+
+        particleEmitter:Destroy()
+
+    end
+
+end
+
+-- Otimize a taxa de atualização do jogo para 60 FPS
+
+runService.Stepped:Connect(function()
+
+    if runService:GetFrameRate() > 60 then
+
+        runService.Heartbeat:Wait()
+
+    end
+
+end)
+
+-- Crie o GUI com o texto em vermelho
+
+local gui = Instance.new("ScreenGui")
+
+local frame = Instance.new("Frame")
+
+frame.Size = UDim2.new(0, 200, 0, 50)
+
+frame.Position = UDim2.new(0.5, -100, 0.5, -25)
+
+frame.BackgroundTransparency = 0.5
+
+frame.BackgroundColor3 = Color3.new(0, 0, 0)
+
+local textLabel = Instance.new("TextLabel")
+
+textLabel.Text = "Created by SUPER VDC"
+
+textLabel.Size = UDim2.new(1, 0, 1, 0)
+
+textLabel.BackgroundTransparency = 1
+
+textLabel.TextColor3 = Color3.new(1, 0, 0)
+
+textLabel.Font = Enum.Font.SourceSans
+
+textLabel.TextSize = 20
+
+textLabel.Parent = frame
+
+frame.Parent = gui
+
+gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+-- Remova o GUI após 7 segundos
+
+wait(7)
+
+gui:Destroy()
